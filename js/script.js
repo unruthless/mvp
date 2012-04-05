376365f21f36e751e01a1855e6ac821a3ebfcbdd
Ruthie BenDor <ruthie@unruthless.com>
2012-04-04 17:43:58 -0700

    First, broken pass at implementing sliding behavior

--- js/script.js
+++ js/script.js
@@ -2,7 +2,26 @@
 
 */
 
+$(document).ready(function(){
+  // On initial load, hide all sections except for the first one
+  $('#guide section').slice(1).addClass('hidden');
+});
 
 
+// When the user clicks the prev link, hide the visible section and show the preceding section
+// TODO: not working
+$('#prev').click(function(event) {
+  event.preventDefault();
+  var currentPage = $('section:not(hidden)');
+  currentPage.addClass('hidden').prev().removeClass('hidden');
+});
 
-
+// When the user clicks the next link, hide the visible section and show the succeeding section
+// TODO: not working
+$('#next').click(function(event) {
+  event.preventDefault();
+  $('section:not(hidden)')
+    .addClass('hidden')
+    .next()
+    .removeClass('hidden');
+});
